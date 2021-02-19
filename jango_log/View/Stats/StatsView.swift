a3f004382bb219a6e18b2d2ba052955ca4a8dc3b
信太　峻 <dashun814@gmail.com>
2021-02-20 02:21:57 +0900

    5分でUITableViewを作ってみる　実装

--- jango_log/View/Stats/StatsView.swift
+++ jango_log/View/Stats/StatsView.swift
@@ -7,10 +7,46 @@
 
 import UIKit
 
-class StatsView: UIViewController {
+class StatsView: UIViewController ,UITableViewDelegate, UITableViewDataSource{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        self.tableView = {
+            let tableView = UITableView(frame: self.view.bounds, style: .plain)
+            tableView.autoresizingMask = [
+              .flexibleWidth,
+              .flexibleHeight
+            ]
+                 tableView.delegate = self
+                tableView.dataSource = self
+
+            self.view.addSubview(tableView)
+
+            return tableView
+
+          }()  }
+   
+    var tableView: UITableView?
+      let items = ["Apple","Banana","Orange"]
+    func numberOfSections(in tableView: UITableView) -> Int {
+      return 1
+    }
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return self.items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+      ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+
+      cell.textLabel?.text = self.items[indexPath.row]
+
+      return cell
+
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      print("Selected! \(self.items[indexPath.row])")
     }
 }
+
